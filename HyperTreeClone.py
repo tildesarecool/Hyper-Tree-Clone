4a01475ac0d7d4c9faea6869aba47700fef5fe83
tildesarecool <tildesare.cool@gmail.com>
2024-07-13 22:24:23 -0700

    Did a lot of refactoring and validating. And some benchmarks with mixed results.

--- HyperTreeClone.py
+++ HyperTreeClone.py
@@ -2,62 +2,63 @@
 # https://github.com/tildesarecool/Hyper-Tree-Clone.git
 # initial start 12 July 2024
 
-import os
-import shutil
+import os, shutil, time, threading, argparse
 import multiprocessing as mp
 from multiprocessing import Pool
-import time
-import ctypes
+#import ctypes
 
+# Flag to control the progress indicator thread
+progress_indicator_running = False
 
 # Hardcoded source and destination directories for development purposes
-SOURCE_DIR = "C:\\Users\\Keith\\Documents\\tiny11\\win11"
+#SOURCE_DIR = "C:\\Users\\Keith\\Documents\\tiny11\\win11"
 #SOURCE_DIR = "C:\\Users\\Keith\\Documents\\New folder\\Win11\\efi"
-DESTINATION_DIR = "e:\\"
+#DESTINATION_DIR = "C:\\Users\\Keith\\Documents\\tiny11\\New Folder"
+#DESTINATION_DIR = "e:\\new folder"
 
 # Constants
 FAT32_MAX_FILE_SIZE = 4 * 1024 * 1024 * 1024  # 4 GB
 
 
-def get_drive_format(drive):
-    """Return the file system format of the given drive."""
+def get_drive_format(path):
+    """Return the file system format of the given path."""
+    drive = os.path.splitdrive(path)[0]
+    if not drive:
+        raise ValueError("Invalid path: no drive letter found.")
     drive_info = os.popen(f'fsutil fsinfo volumeinfo {drive}').read()
     if 'FAT32' in drive_info:
         return 'FAT32'
-    elif 'exFAT' in drive_info:
-        return 'exFAT'
-    return 'UNKNOWN'
-
-
-#def copy_file(file_info):
-#    src, dest = file_info
-#    if os.path.getsize(src) > FAT32_MAX_FILE_SIZE:
-#        print(f"Skipping {src} (exceeds 4 GB)")
-#        return
-#    os.makedirs(os.path.dirname(dest), exist_ok=True)
-#    try:
-#        shutil.copy2(src, dest)
-#    except OSError as e:
-#        print(f"Error copying {src} to {dest}: {e}")
+    return 'OTHER'
 
 def copy_file(file_info):
     src, dest = file_info
     os.makedirs(os.path.dirname(dest), exist_ok=True)
-    shutil.copy2(src, dest)
-
+    shutil.copy2(src, dest)    
+    
+    
 def copy_files(source_dir, destination_dir):
     # Check the destination file system format
     drive_format = get_drive_format(destination_dir)
 
+    # If the destination is FAT32, check for files larger than 4GB
+    if drive_format == 'FAT32':
+        for root, _, files in os.walk(source_dir):
+            for file in files:
+                src_path = os.path.join(root, file)
+                if os.path.getsize(src_path) > FAT32_MAX_FILE_SIZE:
+                    print(f"Error: The file {src_path} exceeds the 4GB limit of FAT32 file system.")
+#                    print(f"Options - Assuming WIM file: \n1. Split the WIM file into two files (beyond the scope of this script). \
+#\n2. Select a destination not formatted with FAT32 and see if WIM file can be shrunk then try copy again.")
+                    print("Copy operation aborted.")
+                    return False
+
+    # Gather all files to be copied
     file_list = []
     for root, _, files in os.walk(source_dir):
         for file in files:
             src_path = os.path.join(root, file)
             rel_path = os.path.relpath(src_path, source_dir)
             dest_path = os.path.join(destination_dir, rel_path)
-            if drive_format == 'FAT32' and os.path.getsize(src_path) > FAT32_MAX_FILE_SIZE:
-                print(f"File {src_path} exceeds FAT32 file size limit and will be skipped.")
-                continue
             file_list.append((src_path, dest_path))
 
     # Determine the number of CPUs to use
@@ -71,23 +72,76 @@ def copy_files(source_dir, destination_dir):
 
     with Pool(processes=cpu_count) as pool:
         pool.map(copy_files_chunk, file_chunks)
-        
+    return True
+
 def copy_files_chunk(file_chunk):
     for file_info in file_chunk:
         copy_file(file_info)
-        
+
+def progress_indicator():
+    """Print a period to indicate progress every 20 seconds."""
+    global progress_indicator_running
+    start_time = time.time()
+    
+    while progress_indicator_running:
+        time.sleep(1)
+        elapsed_time = time.time() - start_time
+        if int(elapsed_time) % 20 == 0:
+            print("#", end="", flush=True)
+
 def main():
     # Copy files from the source directory to the destination directory using multiprocessing
-    print(f"starting file copy...\n")
-    start_time = time.time()
 
-    copy_files(SOURCE_DIR, DESTINATION_DIR)
+    global progress_indicator_running
+
+
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Hyper Tree Clone Utility")
+    parser.add_argument("source", help="Source directory")
+    parser.add_argument("destination", help="Destination directory")
+    args = parser.parse_args()
+
+    source_dir = args.source
+    destination_dir = args.destination
+
+#    source_dir = "\"" + source_dir
+#    source_dir = source_dir + "\""
+    print(f"value of source dir is {source_dir} and file type is {type(source_dir)}")
+
+    start_time = time.time()
+    
+    # progress indicator thread 
+
+    progress_indicator_running = True
+    indicator_thread = threading.Thread(target=progress_indicator)
+    indicator_thread.start()
+
+    try:
+        # start the copy operation
+        print(f"starting file copy...\n")
+        copyOperation = copy_files(source_dir, destination_dir)
+    finally:
+        # stop progress indicator thread
+        progress_indicator_running = False
+        indicator_thread.join()
 
     end_time = time.time()
     elapsed_time = end_time - start_time
-
-
-    print(f"File copying completed in {elapsed_time:.2f} seconds")
     
+    minutes = int(elapsed_time // 60)
+    seconds = elapsed_time % 60
+    if minutes > 0:
+        total_elapsed_time = f"{minutes} minutes and {seconds:.2f} seconds"
+    else:
+        total_elapsed_time = f"{seconds:.2f} seconds"
+
+    if copyOperation:
+        print(f"\nFile copying completed in {total_elapsed_time}.")
+
+
+
 if __name__ == "__main__":
     main()
+#     drive_format = get_drive_format(DESTINATION_DIR)
+#     print(f"value of drive format is {drive_format}")
--- README.md
+++ README.md
@@ -23,3 +23,55 @@ Despite this I decided to continue working on the utility as it could hypothetic
 I've already made one step forward while testing with a thumb drive: take some alternative action when the destination of the copy is FAT32 and the source contains a file exceeding 4 Gigabytes - the max file size of FAT32. 
 
 I re-formatted the drive to exFAT and the test copy with the ~5 gig WIM copied in 773.8 seconds (12.88 minutes). Should probably calculate that as minutes/seconds. I did the same copy with drag-and-drop windows copy and it took 14 minutes 10 seconds. Other variables like contigoiusness of the flash not withstanding. Not scientific, obviously. I'm sure the file group/chunking still had something to do with it. I'm sure I can get it faster.
+
+##### 13 July 2024
+
+I made a quite a lot of progress today. In theory anyway. I tried to add some edge cases and added in support for command line arguments instead of using constants for the tests. I also tried to add in a progress indicator where it just added a # every so many seconds. Just so it's easy to tell stuff is happening and it's not locked up. Not a progress bar, more of a "something is happening indicator". I don't think it's working correctly, though. I may take it out or use something more based on how much has been copied.
+
+Also, I've found a pretty good way to (unscientifically) benchmark the script: I have a USB drive with a directory try I was trying to keep synced across multiple locations. The size of which has ballooned to ~35 gigabytes of files of all sizes. And I already have a prepared robocopy line (as a batch file). 
+
+This will from a USB 3 storage drive (I think it's an NVME inside) to my nvme internal storage device. An empty folder so it's not overwriting anything.
+
+And I don't think it matters but I'm running this via powershell in in Windows Terminal 7.4.3.
+
+Test one with robocopy line:
+
+```robocopy "F:\Windows Install Customization" "P:\Windows Install Customization" /mir  /r:1 /w:1```
+
+Final Time: 
+2 min 3 seconds
+I think. Robocopy output is kind of confusing. Here's the output at the end of the copy, you can be the judge.
+```
+------------------------------------------------------------------------------
+
+               Total    Copied   Skipped  Mismatch    FAILED    Extras
+    Dirs :       938       938         0         0         0         0
+   Files :      7390      7390         0         0         0         0
+   Bytes :  32.590 g  32.590 g         0         0         0         0
+   Times :   0:02:03   0:01:54                       0:00:00   0:00:08
+
+
+   Speed :           304534234 Bytes/sec.
+   Speed :           17425.588 MegaBytes/min.
+   Ended : Saturday, July 13, 2024 17:55:02
+```
+
+
+
+Test one with HyperTreeClone
+
+I'm using a different physical drive as a destination. Still on the root though. There's a few hundred variables I'm not accounting for. But I figure root-to-root is close enough. And also I made sure the C: drive had abundant free storage space like my P: drive. The C: drive has 83 gigs free, ready for 35 gig file copy.
+
+```HyperTreeClone.py "F:\Windows Install Customization" "C:\Windows Install Customization"```
+
+7 minutes 27.36 minutes(?)
+
+Since the two are so far off I'm going to use HyperCloneTree against my P: drive as another test. I deleted the existing *Windows Install Customization* folder and decided to try it again.
+
+Test 2 of HyperTreeClone, command line
+
+```HyperTreeClone.py "F:\Windows Install Customization" "P:\Windows Install Customization"```
+
+2 minutes 7.12 seconds
+
+Well much closer. Apparently my C: drive is not very fast. And the script is barely comparable with robocopy. 
